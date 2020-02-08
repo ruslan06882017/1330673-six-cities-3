c0e9f4297fed2beacba905d2f1c994aa9dc6cb09
Ruslan G <r.gusseinov@mail.ru>
2020-02-08 19:53:27 +0600

    added react code for project six-cities

---
+++ src/components/main/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Main = (props) => {
+    // eslint-disable-next-line react/prop-types
+    const {numberofRent} = props;  
+    return (
+        <div> Количество предложении {numberofRent} </div>
+    );
+}
+export default Main;
