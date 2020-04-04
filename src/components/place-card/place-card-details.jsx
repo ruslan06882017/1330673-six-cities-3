da0df431d42ef323fa3be6b174b57c82843c7c57
Ruslan Gusseinov <r.gusseinov@mail.ru>
2020-04-04 14:05:47 +0600

    Output the dynamic values in place card comp

--- src/components/place-card/place-card-details.jsx
+++ src/components/place-card/place-card-details.jsx
@@ -3,11 +3,9 @@ import PropTypes from "prop-types";
 
 const PlaceCardDetails = (props) => {
   const {place} = props;
-  console.log(props);
-  const {name} = place;
-  
-
-
+  const {name, amount, amountDescription, rating, type, isPremium, isInBookmark, description, image} = place;
+  const {photos, features, insideItems} = place;
+ 
   return (
     <div className="page">
       <header className="header">
@@ -24,7 +22,7 @@ const PlaceCardDetails = (props) => {
                   <a className="header__nav-link header__nav-link--profile" href="#">
                     <div className="header__avatar-wrapper user__avatar-wrapper">
                     </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com {name}</span>
+                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
                   </a>
                 </li>
               </ul>
@@ -37,97 +35,62 @@ const PlaceCardDetails = (props) => {
         <section className="property">
           <div className="property__gallery-container container">
             <div className="property__gallery">
-              <div className="property__image-wrapper">
-                <img className="property__image" src="img/room.jpg" alt="Photo studio"/>
-              </div>
-              <div className="property__image-wrapper">
-                <img className="property__image" src="img/apartment-01.jpg" alt="Photo studio"/>
-              </div>
-              <div className="property__image-wrapper">
-                <img className="property__image" src="img/apartment-02.jpg" alt="Photo studio"/>
-              </div>
-              <div className="property__image-wrapper">
-                <img className="property__image" src="img/apartment-03.jpg" alt="Photo studio"/>
-              </div>
-              <div className="property__image-wrapper">
-                <img className="property__image" src="img/studio-01.jpg" alt="Photo studio"/>
-              </div>
-              <div className="property__image-wrapper">
-                <img className="property__image" src="img/apartment-01.jpg" alt="Photo studio"/>
-              </div>
+              {photos.map((value) => (
+                <div className="property__image-wrapper">
+                  <img className="property__image" src={value} alt="Photo studio"/>
+                </div>
+              ))}
             </div>
           </div>
+          
           <div className="property__container container">
             <div className="property__wrapper">
               <div className="property__mark">
-                <span>Premium</span>
+                <span>
+                  {(isPremium) ? 'Premium' : 'Medium'}
+                </span>
               </div>
               <div className="property__name-wrapper">
                 <h1 className="property__name">
-                  Offer name is here
+                  {name}
                 </h1>
-                <button className="property__bookmark-button button" type="button">
-                  <svg className="property__bookmark-icon" width="31" height="33">
-                    <use xlinkHref="#icon-bookmark"></use>
-                  </svg>
-                  <span className="visually-hidden">To bookmarks</span>
-                </button>
+                {(isInBookmark) ? (
+                  <button className="property__bookmark-button button" type="button">
+                    <svg className="property__bookmark-icon" width="31" height="33">
+                      <use xlinkHref="#icon-bookmark"></use>
+                    </svg>
+                    <span className="visually-hidden">To bookmarks</span>
+                  </button>
+                ) : null}
               </div>
               <div className="property__rating rating">
                 <div className="property__stars rating__stars">
                   <span style={{width: `80%`}}></span>
                   <span className="visually-hidden">Rating</span>
                 </div>
-                <span className="property__rating-value rating__value">4.8</span>
+                <span className="property__rating-value rating__value"> {rating}</span>
               </div>
               <ul className="property__features">
                 <li className="property__feature property__feature--entire">
-                  Apartment
+                  {features[`type`]}
                 </li>
                 <li className="property__feature property__feature--bedrooms">
-                  3 Bedrooms
+                 {features[`bedrooms`]} Bedrooms
                 </li>
                 <li className="property__feature property__feature--adults">
-                  Max 4 adults
+                  Max {features[`maxGuests`]} Adults
                 </li>
               </ul>
               <div className="property__price">
-                <b className="property__price-value">&euro;120</b>
-                <span className="property__price-text">&nbsp;night</span>
+                <b className="property__price-value">&euro;{amount}</b>
+                <span className="property__price-text">&nbsp;{amountDescription}</span>
               </div>
               <div className="property__inside">
                 <h2 className="property__inside-title">What&apos;s inside</h2>
                 <ul className="property__inside-list">
-                  <li className="property__inside-item">
-                    Wi-Fi
-                  </li>
-                  <li className="property__inside-item">
-                    Washing machine
-                  </li>
-                  <li className="property__inside-item">
-                    Towels
-                  </li>
-                  <li className="property__inside-item">
-                    Heating
-                  </li>
-                  <li className="property__inside-item">
-                    Coffee machine
-                  </li>
-                  <li className="property__inside-item">
-                    Baby seat
-                  </li>
-                  <li className="property__inside-item">
-                    Kitchen
-                  </li>
-                  <li className="property__inside-item">
-                    Dishwasher
-                  </li>
-                  <li className="property__inside-item">
-                    Cabel TV
-                  </li>
-                  <li className="property__inside-item">
-                    Fridge
-                  </li>
+                  {insideItems.map((item) => (
+                      <li className="property__inside-item">{item}</li>
+                  ))}
                 </ul>
               </div>
               <div className="property__host">
